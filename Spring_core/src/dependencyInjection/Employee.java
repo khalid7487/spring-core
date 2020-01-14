f72dcfdc893f52acb852ec71f3f9f7dee72e881d
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-14 22:03:16 +0600

    dependency injection with another class

--- Spring_core/src/dependencyInjection/Employee.java
+++ Spring_core/src/dependencyInjection/Employee.java
@@ -12,25 +12,17 @@ package dependencyInjection;
 public class Employee {
     private int id;
     private String name;
+    private Address address;
 
-    public Employee() {
-    }
-
-    public Employee(int id) {
-        this.id = id;
-    }
-
-    public Employee(String name) {
-        this.name = name;
-    }
-
-    public Employee(int id, String name) {
+    public Employee(int id, String name, Address address) {
         this.id = id;
         this.name = name;
-    }
+        this.address = address;
+    } 
     
     public void show(){
         System.out.println(id+" : "+name);
+        System.out.println(address);
     }
             
 }
