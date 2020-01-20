9a416a31208153e5c43a526c24fb42097a3e4c31
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-20 20:05:41 +0600

    pointcut

--- SpringAop/untitled/src/aspect/EMpAspect.java
+++ SpringAop/untitled/src/aspect/EMpAspect.java
@@ -2,14 +2,24 @@ package aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class EMpAspect {
 
-    @Before("execution(public *  get*())")
+    @Before("allGetters()")
     public void beforeAdvice(){
         System.out.println("Aspect run");
     }
+
+    @Before("allGetters()")
+    public void secondAdvice(){
+        System.out.println("Second run");
+    }
+
+    @Pointcut("execution(public * get*(..))")
+    public void allGetters(){}
+
 }
