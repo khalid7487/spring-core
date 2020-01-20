2cd41cc7cdc91e438444cec6f9212f6a81f39441
khalid7487 <khalid15-7487@diu.edu.bd>
2020-01-20 20:36:22 +0600

    prameter use in advice

--- SpringAop/untitled/src/aspect/EMpAspect.java
+++ SpringAop/untitled/src/aspect/EMpAspect.java
@@ -17,6 +17,11 @@ public class EMpAspect {
         Manager manager=(Manager)joinPoint.getTarget();
     }
 
+    @Before("args(name)")
+    public void stringArgMethods(String name){
+        System.out.println("Methods run with String args with value: "+name);
+    }
+
 //    @Before("allGetters()")
 //    public void secondAdvice(){
 //        System.out.println("Second run");
